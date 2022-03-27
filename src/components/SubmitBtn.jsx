1877cf4c427c86d1f07e5b7f378eee40a061fcdc
Agastya <agastyarajawat909@gmail.com>
2022-03-27 22:33:28 +0530

    Create submit button

---
+++ src/components/SubmitBtn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SubmitBtn = () => {
+  return (
+    <button
+      type="submit"
+      className="text-lg p-1 m-2 text-gray-800 self-end bg-green-300 rounded-md text-center w-36"
+    >
+      Save
+    </button>
+  );
+};
+
+export default SubmitBtn;
