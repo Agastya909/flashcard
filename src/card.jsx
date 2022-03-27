9a6fc52db0d0d4000c4fd37aad238a5cce71fcab
Agastya <agastyarajawat909@gmail.com>
2022-03-27 18:16:22 +0530

    add link to edit

--- src/card.jsx
+++ src/card.jsx
@@ -1,7 +1,15 @@
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Link, Navigate } from "react-router-dom";
+import { useState } from "react";
+
 const Card = (props) => {
-  const { notetitle, notedata, notecolor, id } = props;
+  const { notetitle, notedata, notecolor, id, notepriority } = props;
+  const [oneNote, setOneNote] = useState({
+    noteTitle: "",
+    noteData: "",
+    notePriority: "",
+    noteColor: "",
+  });
 
   const deleteNote = () => {
     console.log(`note title is : ${notetitle} and notecolor is : ${notecolor}`);
@@ -15,12 +23,19 @@ const Card = (props) => {
       });
   };
 
-  const updateNote = () => {
-    console.log(`Update function ${id}`);
+  const handleClick = () => {
+    console.log(oneNote);
   };
 
-  const handleClick = () => {
-    console.log(`title ${notetitle} --  color ${notecolor} `);
+  const UpdateNote = () => {
+    const updateData = {
+      noteTitle: notetitle,
+      noteColor: notecolor,
+      noteData: notedata,
+      notePriority: notepriority,
+    };
+    setOneNote(updateData);
+    console.log(updateData);
   };
 
   return (
@@ -37,6 +52,7 @@ const Card = (props) => {
         <div>{notetitle}</div>
         <div className="flex flex-row">
           <button
+            name="deleteButton"
             type="button"
             onClick={deleteNote}
             className="p-1  hover:bg-red-200 hover:text-black rounded-md"
@@ -53,10 +69,16 @@ const Card = (props) => {
               <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
             </svg>
           </button>
-          <Link to={"/update"}>
+          <Link
+            to={{
+              pathname: `/update/${id}`,
+              state: oneNote,
+            }}
+          >
             <button
+              name="updateButton"
               type="button"
-              onClick={updateNote}
+              // onClick={UpdateNote}
               className="font-semibold text-xl p-1  hover:bg-blue-400 hover:text-black rounded-md"
             >
               <svg
